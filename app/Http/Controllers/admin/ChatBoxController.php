1caa82a017a1ab9b9d53ce3589d02fddce1d4810
unknown <leo112071@gmail.com>
2018-03-08 15:12:35 +0700

    Chat admin

--- app/Http/Controllers/admin/ChatBoxController.php
+++ app/Http/Controllers/admin/ChatBoxController.php
@@ -18,14 +18,31 @@ class ChatBoxController extends Controller
           array_push($data['chat_users'], $temp);
       }
 
+      if (isset($_GET['user'])) {
+          $data['sender'] = $_GET['user'];
+          $chats = Chat::select('partner_id')
+                       ->where('user_id', $data['sender'])
+                       ->distinct()
+                       ->get();
+          $data['receivers'] = array();
+          foreach ($chats as $chat) {
+            array_push($data['receivers'], User::find($chat->partner_id));
+          }
+      }
     	return view('admin.chatbox.index', $data);
     }
 
     public function find() {
-        $id = $_GET['id'];
-        $data['chats'] = Chat::where('user_id', $id)->orWhere('partner_id', $id)
-                                                    ->get();
-        $data['sender'] = $id;
+        $sender = $_GET['sender'];
+        $receiver = $_GET['receiver'];
+        $data['sender'] = User::find($sender);
+        $data['receiver'] = User::find($receiver);
+        $data['chats'] = Chat::where('user_id', $sender)
+                            ->orWhere('partner_id', $receiver)
+                            ->orWhere('partner_id', $sender)
+                            ->orWhere('user_id', $receiver)
+                            ->get();
+        
         return view('admin.chatbox.find', $data);
     }
 }
--- resources/views/admin/chatbox/find.blade.php
+++ resources/views/admin/chatbox/find.blade.php
@@ -1,11 +1,42 @@
 @extends("admin.menu")
+<style type="text/css">
+	table {
+		border: none;
+		width: 100%;
+	}
 
+	#Penerima {
+		float: right;
+		text-align: right;
+	}
+</style>
 @section('content')
-    <div id="chatbox">
-      @foreach($chats as $chat)
-        <div id=@if($sender == $chat->user_id) "sent" @else "received" @endif>
-          {{$chat->chat}}
-        </div>
-      @endforeach
-    </div>
+	<div class="row">
+		<div class="col-sm-3">
+			<div id="Pengirim">
+				<h3>Pengirim </h3>
+				<strong>{{$sender->name}}</strong><br>
+				{{$sender->email}}<br>
+				{{$sender->phone}}
+			</div>
+		</div>
+
+		<div class="col-sm-6">
+			<div id="chatbox">
+		      @foreach($chats as $chat)
+		        <div id=@if($sender->id == $chat->user_id) "sent" @else "received" @endif>
+		          {{$chat->chat}}
+		        </div>
+		      @endforeach
+		    </div>
+		</div>
+
+		<div class="col-sm-3">
+			<div id="Penerima">
+				<h3>Penerima </h3>
+				<strong>{{$receiver->name}}</strong><br>
+				{{$receiver->email}}<br>
+				{{$receiver->phone}}
+			</div>
+		</div>
 @endsection
